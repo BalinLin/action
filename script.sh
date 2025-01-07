50ba4120d35e8e1966f648366256700a5fdfbbbd
BalinLin <giveme2486@gmail.com>
2025-01-07 16:04:42 +0800

    Test GITHUB_STEP_SUMMARY newline

--- script.sh
+++ script.sh
@@ -3,3 +3,10 @@
 @echo "SETTER=echo_hello_world" >> $GITHUB_ENV
 echo "SETTER=echo_hello_world" >> $GITHUB_ENV
 echo '### Hello world from echo_hello_world 🚀' >> $GITHUB_STEP_SUMMARY
+echo 'line 1' >> $GITHUB_STEP_SUMMARY
+echo 'line 2' >> $GITHUB_STEP_SUMMARY
+echo '- indentation 1' >> $GITHUB_STEP_SUMMARY
+echo 'After indentation 1' >> $GITHUB_STEP_SUMMARY
+echo '- indentation 2' >> $GITHUB_STEP_SUMMARY
+echo '' >> $GITHUB_STEP_SUMMARY
+echo 'After indentation 2' >> $GITHUB_STEP_SUMMARY
